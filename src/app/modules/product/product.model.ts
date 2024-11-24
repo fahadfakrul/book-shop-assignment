41b874d6060fa89e8dcffb8a3b5a7e5267204893
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-11-25 02:14:25 +0600

    model error message added

--- src/app/modules/product/product.model.ts
+++ src/app/modules/product/product.model.ts
@@ -3,17 +3,21 @@ import { Product } from './product.interface';
 
 const productSchema = new Schema<Product>(
   {
-    title: { type: String, required: true },
-    author: { type: String, required: true },
+    title: { type: String, required: [true, 'Title is required'] },
+    author: { type: String, required: [true, 'Author is required'] },
     price: { type: Number, required: true, min: 0 },
     category: {
       type: String,
       enum: ['Fiction', 'Science', 'SelfDevelopment', 'Poetry', 'Religious'],
-      required: true,
+      required: [true, 'Category is required'],
     },
     description: { type: String },
-    quantity: { type: Number, required: true, min: 0 },
-    inStock: { type: Boolean, required: true },
+    quantity: {
+      type: Number,
+      required: [true, 'quantity is required'],
+      min: 0,
+    },
+    inStock: { type: Boolean, required: [true, 'inStock status is required'] },
   },
   { timestamps: true },
 );
